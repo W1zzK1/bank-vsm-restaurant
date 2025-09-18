ca1a9379d6adc263ece09058b8f744c44e0921bd
Vladislav Gorbunov <w1zzard.solo@yandex.ru>
2025-09-18 21:53:07 +0300

    add basic http route

--- app/main.go
+++ app/main.go
@@ -4,9 +4,14 @@ import (
     "bank-vsm-restaurant/app/payment"
     "net/http"
     "log"
+    "fmt"
 )
 
 func main() {
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Bank API is running!")
+	})
       
     http.HandleFunc("/api/v1/payments", payment.PaymentHandler)
     

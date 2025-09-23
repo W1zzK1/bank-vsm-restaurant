c3c7f0e4b084a08e3b92984ad9df28c660f99a7e
Vladislav Gorbunov <w1zzard.solo@yandex.ru>
2025-09-23 16:13:43 +0300

    webhook response to json

--- app/main.go
+++ app/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-    "bank-vsm-restaurant/app/payment"
-    "net/http"
-    "log"
-    "fmt"
+	"bank-vsm-restaurant/app/payment"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
@@ -16,11 +17,18 @@ func main() {
     http.HandleFunc("/api/v1/payment",  payment.PaymentHandlerWithHTML)
 
     http.HandleFunc("/payment/success", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Спасибо! Ваш платеж успешно обработан.")
-	})
+		resp := map[string]int32{"payment_status": 200} // Success
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+		http.Error(w, "failed to encode response", http.StatusInternalServerError)
+		return
+	}})
+
 	http.HandleFunc("/payment/fail", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Произошла ошибка или вы отменили платеж. Попробуйте снова.")
-	})
+		resp := map[string]int32{"payment_status": 400} // Decline or fail
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			http.Error(w, "failed to encode response", http.StatusInternalServerError)
+			return
+		}})
     
     log.Println("Server is starting on port :8181...")
 	err := http.ListenAndServe(":8181", nil)
